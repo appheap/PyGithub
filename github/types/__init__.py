3cad3eaaa801ced117c4ff0178f6d87ee647f24a
sigma1326 <taleb.zarhesh@gmail.com>
2022-02-28 14:51:05 +0330

    + Add `requests` package to the project dependencies + Update `types` package

---
+++ github/types/__init__.py
@@ -0,0 +1,18 @@
+from .github_object import GithubObject
+from .headers import Headers
+from .license import License
+from .list import List
+from .object import Object
+from .repo_permissions import RepoPermissions
+from .repository import Repository
+
+__all__ = [
+    'GithubObject',
+    'Headers',
+    'License',
+    'List',
+    'Object',
+    'RepoPermissions',
+    'Repository',
+
+]
