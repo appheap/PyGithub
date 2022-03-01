a6ac4495a6d4c7f6cd3635267236273340d11ef9
sigma1326 <taleb.zarhesh@gmail.com>
2022-03-01 22:24:04 +0330

    + Add `get_org_repositories` module to `github/methods/repos/` package + Update `get_user_repositories` module in `github/methods/repos/` package

--- github/methods/repos/__init__.py
+++ github/methods/repos/__init__.py
@@ -1,10 +1,12 @@
 from .get_my_repositories import GetMyRepositories
 from .get_user_repositories import GetUserRepositories
+from .get_org_repositories import GetOrgRepositories
 
 
 class Repos(
     GetMyRepositories,
     GetUserRepositories,
+    GetOrgRepositories,
 
 ):
     pass
--- github/methods/repos/get_org_repositories.py
+++ github/methods/repos/get_org_repositories.py
@@ -4,11 +4,11 @@ from github.scaffold import Scaffold
 from github.types import Repository
 
 
-class GetUserRepositories(Scaffold):
-    def get_user_repositories(
+class GetOrgRepositories(Scaffold):
+    def get_org_repositories(
             self,
             *,
-            username: str = None,
+            organization: str = None,
             type: str = 'all',
             sort: str = 'updated',
             direction: str = 'desc',
@@ -16,21 +16,24 @@ class GetUserRepositories(Scaffold):
             page: int = None,
     ) -> Optional[List['Repository']]:
         """
-        Lists public repositories for the specified user. Note: For GitHub AE, this endpoint will list internal repositories for the specified user.
+        Lists repositories for the specified organization.
 
-        :param username:
-            Username of the User
+        :param organization:
+            Name of the Organization
 
         :param type:
-            Can be one of "all", "owner", "member".
-            Default: "owner"
+            Specifies the types of repositories you want returned. Can be one of "all", "public", "private", "forks", "sources", "member", "internal".
+            Note: For GitHub AE, can be one of "all", "private", "forks", "sources", "member", "internal".
+            Default: "all".
+            If your organization is associated with an enterprise account using GitHub Enterprise Cloud or GitHub Enterprise Server 2.20+, type can also be internal.
+            However, the internal value is not yet supported when a GitHub App calls this API with an installation access token.
 
         :param sort:
             Can be one of "created", "updated", "pushed", "full_name".
-            Default: "full_name"
+            Default: "created"
 
         :param direction:
-            Can be one of "asc" or "desc". Default: "asc" when using "full_name", otherwise "desc"
+            Can be one of "asc" or "desc". Default: when using "full_name": "asc", otherwise "desc"
 
         :param per_page:
             Results per page (max "100")
@@ -43,7 +46,7 @@ class GetUserRepositories(Scaffold):
         :return: List[`types.Repository`]
         """
         response = self.get_with_token(
-            url=f'https://api.github.com/users/{username}/repos',
+            url=f'https://api.github.com/orgs/{organization}/repos',
             params={
                 'type': type,
                 'sort': sort,
