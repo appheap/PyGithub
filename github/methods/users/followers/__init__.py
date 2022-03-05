91ff8a2bcce7190c79c6cf1b82d17dc8d460bcb3
sigma1326 <taleb.zarhesh@gmail.com>
2022-03-06 01:31:56 +0330

    + Add `follow_user` module to `github/methods/users/followers/` package

--- github/methods/users/followers/__init__.py
+++ github/methods/users/followers/__init__.py
@@ -1,11 +1,14 @@
 from .get_followers import GetFollowers
 from .get_following import GetFollowing
 from .is_user_followed import IsUserFollowed
+from .follow_user import FollowUser
+
 
 class Followers(
     GetFollowers,
     GetFollowing,
     IsUserFollowed,
+    FollowUser,
 
 ):
     pass
---
+++ github/methods/users/followers/follow_user.py
@@ -0,0 +1,38 @@
+from typing import Tuple, List, Union
+
+from github.scaffold import Scaffold
+from github.types import SimpleUser, Response
+
+
+class FollowUser(Scaffold):
+    """
+    Follow a user
+    """
+
+    def follow_user(
+            self,
+            *,
+            username: str,
+    ) -> Tuple['bool', Union['bool', 'Response']]:
+        """
+        Follow a user
+
+        Following a user requires the user to be logged in and authenticated with basic auth or OAuth with the `user:follow` scope.
+
+
+        :param username: Username of the user to be followed
+
+
+        :return: Tuple['bool', Union['bool', 'Response']]
+        """
+        response = self.get_with_token(
+            url=f'https://api.github.com/user/following/{username}',
+        )
+        if response.status_code in (204, 304):
+            # Status: 204 No Content
+            # Status: 304 Not Modified => the person is already followed by the authenticated user
+            return True, True
+        elif response.status_code in (404, 403, 401):
+            return False, Response._parse(response.status_code, response.json(), getattr(response, 'message', None))
+        else:
+            return False, Response._parse(response.status_code, response.json(), getattr(response, 'message', None))
