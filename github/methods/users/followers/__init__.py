62c3a1563f0c471d04cb6cdbe4c601373cba318b
sigma1326 <taleb.zarhesh@gmail.com>
2022-03-06 01:37:58 +0330

    + Add `unfollow_user` module to `github/methods/users/followers/` package

--- github/methods/users/followers/__init__.py
+++ github/methods/users/followers/__init__.py
@@ -2,6 +2,7 @@ from .get_followers import GetFollowers
 from .get_following import GetFollowing
 from .is_user_followed import IsUserFollowed
 from .follow_user import FollowUser
+from .unfollow_user import UnfollowUser
 
 
 class Followers(
@@ -9,6 +10,7 @@ class Followers(
     GetFollowing,
     IsUserFollowed,
     FollowUser,
+    UnfollowUser,
 
 ):
     pass
