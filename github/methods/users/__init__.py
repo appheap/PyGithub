297035f17a2c5c00ece1a2378721d15c0f41d37f
sigma1326 <taleb.zarhesh@gmail.com>
2022-03-05 23:37:39 +0330

    + Add `get_followers` module to `github/methods/users/followers/` package

--- github/methods/users/__init__.py
+++ github/methods/users/__init__.py
@@ -1,6 +1,9 @@
 from .get_authenticated_user_info import GetAuthenticatedUserInfo
+from .followers import Followers
+
 
 class Users(
     GetAuthenticatedUserInfo,
+    Followers,
 ):
     pass
---
+++ github/methods/users/followers/__init__.py
@@ -0,0 +1,8 @@
+from .get_followers import GetFollowers
+
+
+class Followers(
+    GetFollowers,
+
+):
+    pass
