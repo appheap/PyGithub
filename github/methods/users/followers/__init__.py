a8b9433dcc170000765b965cb44a81d5fa7ba24e
sigma1326 <taleb.zarhesh@gmail.com>
2022-03-05 23:54:16 +0330

    + Add `get_following` module to `github/methods/users/followers/` package

--- github/methods/users/followers/__init__.py
+++ github/methods/users/followers/__init__.py
@@ -1,8 +1,10 @@
 from .get_followers import GetFollowers
+from .get_following import GetFollowing
 
 
 class Followers(
     GetFollowers,
+    GetFollowing,
 
 ):
     pass
---
+++ github/methods/users/followers/get_following.py
@@ -0,0 +1,45 @@
+from typing import Tuple, List, Union
+
+from github.scaffold import Scaffold
+from github.types import SimpleUser, Response
+
+
+class GetFollowing(Scaffold):
+    """
+    List the people the authenticated user follows
+    """
+
+    def get_following(
+            self,
+            *,
+            per_page: int = 100,
+            page: int = 1
+    ) -> Tuple['bool', Union[List['SimpleUser'], 'Response']]:
+        """
+        Lists the people who the authenticated user follows.
+
+
+        :param per_page: Results per page (max 100) Default: 30
+
+        :param page: Page number of the results to fetch. Default: 1
+
+        :return: Tuple['bool', Union[List['SimpleUser'], 'Response']]
+        """
+        response = self.get_with_token(
+            url=f'https://api.github.com/user/following',
+            params={
+                'per_page': per_page,
+                'page': page,
+            }
+        )
+        if response.status_code == 200:
+            users = []
+            for user_dict in response.json():
+                user = SimpleUser._parse(user_dict)
+                if user_dict is not None and len(user_dict):
+                    users.append(user)
+            return True, users
+        elif response.status_code in (304, 403, 401):
+            return False, Response._parse(response.status_code, response.json(), getattr(response, 'message', None))
+        else:
+            return False, Response._parse(response.status_code, response.json(), getattr(response, 'message', None))
