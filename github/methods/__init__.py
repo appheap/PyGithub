936f3b55054233918f523e6eb9e510249201abe2
sigma1326 <taleb.zarhesh@gmail.com>
2022-03-14 20:31:48 +0330

    + Add `rate_limit` package to `github/methods/` package

--- github/methods/__init__.py
+++ github/methods/__init__.py
@@ -2,6 +2,7 @@ from .activity import Activity
 from .auth import Auth
 from .base import Base
 from .organizations import Organizations
+from .rate_limit import RateLimit
 from .repos import Repos
 from .search import Search
 from .users import Users
@@ -12,6 +13,7 @@ class Methods(
     Auth,
     Base,
     Organizations,
+    RateLimit,
     Repos,
     Search,
     Users
